65918043161c7817c8b558d3c491fd9a6c9e272b
Polina <sloboda.polina@gmail.com>
2019-03-29 23:11:41 +0300

    вывожу сверху цитату дня

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ $(document).ready(function() {
     document.getElementsByTagName('body')[0].appendChild(canvas);
     var context = canvas.getContext('2d');
 
+    var promises = [];
     for(var i = 0; i < 2; i++) {
       for(var j = 0; j < 2; j++) {
-        drawImage('test.jpg', CANVAS_SIZE * i / 2, CANVAS_SIZE * j / 2);
+        promises.push(
+          drawImage('test.jpg', CANVAS_SIZE * i / 2, CANVAS_SIZE * j / 2)
+        );
         /*
         $.ajax({
           url: "https://api.unsplash.com/photos/random?client_id=" + my_access_key,
@@ -24,21 +27,47 @@ $(document).ready(function() {
         */
       }
     }
+    Promise.all(promises).then(function() {
+        $.ajax({
+          url: "https://quotes.rest/qod",
+          Accept: "application/json",
+          success: function(data) {
+            console.log("SUCCESS!");
+            console.log(data.contents.quotes[0].quote);
+
+            context.font = "40px Georgia";
+            context.textAlign = "center";
+            context.textBaseline = "middle";
+            context.fillText(
+              data.contents.quotes[0].quote,
+              CANVAS_SIZE / 2,
+              CANVAS_SIZE / 2
+            );
+          },
+          error: function(data) {
+            console.log("ERROR!");
+            console.log(data.statusText);
+          }
+        });
+    });
 
     //src of image, x and y position in the canvas
     function drawImage(src, x, y) {
-      var image = new Image();
-      image.src = src;
-      image.onload = function() {
-        context.drawImage(
-          image,
-          0, 0, image.width, image.height,
-          x, y, 400, 400
-        );
-      }
-      image.onerror = function(error) {
-        console.error(error);
-      }
+      return new Promise(function (resolve) {
+        var image = new Image();
+        image.src = src;
+        image.onload = function() {
+          context.drawImage(
+            image,
+            0, 0, image.width, image.height,
+            x, y, 400, 400
+          );
+          resolve();
+        }
+        image.onerror = function(error) {
+          console.error(error);
+        }
+      });
   }
 
 });
